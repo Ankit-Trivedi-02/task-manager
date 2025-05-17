3e3f31c9e36a5da3c10dfa284a2034e2881c6cc0
Ankit Trivedi <ankittrivedi0210@gmail.com>
2025-05-17 17:31:55 +0530

    Tested user registration Handled duplicate email registration route and verified MongoDB entry

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,30 @@
 const user = require("../models/User")
 
 async function registerUser(req, res) {
-    if (!(req.body)) return res.json({ error: "All fields are required! " });
-      const {username,email,password} = req.body;
-      if(!username){
-        return res.status(400).json({ error: "Username is required! "});
-      }
-      if(!email){
-        return res.status(400).json({ error: "valid email is required! "});
-      }
-      if(!password || password.length<8){
-        return res.status(400).json({ error: "password of min length 8 char is required! "});
-      }
-    try {
-            const createdUser = await user.create({username,email,password})
-            return res.status(201).json({success:"user created sucessfully"});
-        }
-        catch (err) {
-            res.json({ error: err })
-        }
+  if (!(req.body)) return res.json({ error: "All fields are required! " });
+  const { username, email, password } = req.body;
+  if (!username) {
+    return res.status(400).json({ error: "Username is required! " });
+  }
+  if (!email) {
+    return res.status(400).json({ error: "valid email is required! " });
+  }
+  if (!password || password.length < 8) {
+    return res.status(400).json({ error: "password of min length 8 char is required! " });
+  }
+
+  const isDoubleMail = await user.findOne({email})
+  if(isDoubleMail){
+     return res.status(400).json({ error: "User already exists with this email" });
+  }
+
+  try {
+    const createdUser = await user.create({ username, email, password })
+    return res.status(201).json({ success: "user created sucessfully" });
+  }
+  catch (err) {
+    res.json({ error: err })
+  }
 }
 
-module.exports= {registerUser};
+module.exports = { registerUser };
