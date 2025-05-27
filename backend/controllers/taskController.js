e7fe154ef4759aff4fe0bde364b71a41564b87e9
Ankit Trivedi <ankittrivedi0210@gmail.com>
2025-05-27 18:49:15 +0530

    Added task update and delete functionality with auth check

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,4 +1,5 @@
 const task = require("../models/Task");
+//const { findOne, findOneAndDelete, findOneAndUpdate } = require("../models/User");
 
 async function allTask(req, res) {
     try {
@@ -28,4 +29,38 @@ async function registerTask(req, res) {
     }
 }
 
-module.exports = { allTask, registerTask };
+async function updateTask(req, res) {
+    if (!req.body || typeof req.body.completed !== 'boolean') {
+        return res.status(400).json({ error: "'completed' must be provided and must be boolean" });
+    }
+    const { completed } = req.body;
+    console.log(completed);
+    const id = req.params.id;
+    try {
+        console.log(req.user._id, id, completed);
+        const updatableTask = await task.findOneAndUpdate(
+            { _id: req.params.id, user: req.user._id },
+            { completed },
+            { new: true });
+        console.log(updatableTask);
+        res.status(200).json({ updatableTask });
+    }
+    catch (error) {
+        res.json({ error: "Error updating your task" })
+    }
+}
+
+
+async function deleteTask(req, res) {
+    const id = req.params.id;
+    try {
+        const deletedTask = await task.findOneAndDelete({ user: req.user._id, _id: id });
+        res.status(200).json({ deletedTask });
+    }
+    catch (error) {
+        res.json({ error: "Error updating your task" })
+    }
+}
+
+
+module.exports = { updateTask, deleteTask, allTask, registerTask };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router();
-const {allTask,registerTask}=require("../controllers/taskController")
+const { updateTask, deleteTask, allTask, registerTask } = require("../controllers/taskController")
 
-router.get("/",allTask);
-router.post("/",registerTask)
+router.get("/", allTask);
+router.post("/", registerTask)
+router.patch("/:id", updateTask)
+router.delete("/:id", deleteTask)
 
-module.exports=router;
+module.exports = router;
